069709df473ac2ec77c8f236d994b15adb12cd10
Swetabja Hazra <13stabjahazra@gmail.com>
2024-08-18 11:44:54 +0530

    Update ProductReviewDialog.jsx

--- src/Components/ProductReviewDialog/ProductReviewDialog.jsx
+++ src/Components/ProductReviewDialog/ProductReviewDialog.jsx
@@ -22,7 +22,7 @@ const ProductReviewDialog = (props) => {
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
           Let Google help apps determine location. This means sending anonymous
-          location data to Google, even when no apps are runningg.
+          location data to Google, even when no apps are running.
         </DialogContentText>
       </DialogContent>
       <DialogActions>
