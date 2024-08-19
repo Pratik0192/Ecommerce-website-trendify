6aed05c35653a19278db978096d0ba4d96d10706
SwetabjaGit <13stabjahazra@gmail.com>
2024-08-19 09:56:57 +0530

    Navbar changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="nav-logo">
-            <img src={logo} alt="" />
+        <img src={logo} alt="" />
         </div>
         <img className='nav-dropdown' onClick={dropdown_toggle} src={dropdown_icon} alt="" />
         <ul ref={menuRef} className="nav-menu">
