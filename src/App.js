bb440bcee251bb9d167365632f724b92880f0022
sayantansardar123 <sayantanardar2003@gmail.com>
2024-09-01 11:58:06 +0530

    app.js  update

--- src/App.js
+++ src/App.js
@@ -14,31 +14,34 @@ import Footer from './Components/Footer/Footer';
 import men_banner from './Components/Assets/banner_mens.png';
 import women_banner from './Components/Assets/banner_women.png';
 import kid_banner from './Components/Assets/banner_kids.png';
-import ShopProvider from './Context/ShopContext'; // Import the context provider
+// import ShopProvider from './Context/ShopContext'; // Import the context provider
 
 function App() {
   return (
-    <ShopProvider>
+    <div> 
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
-          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
-          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
-          <Route path='/laptop' element={<ShopCategory banner={kid_banner} category="laptop" />} />
-          <Route path='/mobile&tablet' element={<ShopCategory banner={kid_banner} category="mobile&tablet" />} />
-          <Route path='/product' element={<Product />} />
-          <Route path='/product/:productId' element={<Product />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-          <Route path='/checkout/:section' element={<Checkout section="shipping" />} />
-          <Route path='/login' element={<LoginSignup authTab="Login" />} />
-          <Route path='/signup' element={<LoginSignup authTab="SignUp" />} />
-        </Routes>
-        <Footer />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
+        <Route path='/laptop' element={<ShopCategory banner={kid_banner} category="laptop"/>}/>
+        <Route path='/mobile&tablet' element={<ShopCategory banner={kid_banner} category="mobile&tablet"/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path='/checkout/:section' element={<Checkout section="shipping"/>}/>
+        {/* <Route path='/checkout/confirm' element={<Checkout section="confirm"/>}/>
+        <Route path='/checkout/payment' element={<Checkout section="payment"/>}/> */}
+        <Route path='/login' element={<LoginSignup authTab="Login"/>}/>
+        <Route path='/signup' element={<LoginSignup authTab="SignUp"/>}/>
+      </Routes>
+      <Footer/>
       </BrowserRouter>
-    </ShopProvider>
+    </div>
   );
 }
 
