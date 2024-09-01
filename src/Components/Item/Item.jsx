d44990d1369b75321209aaa2c7067d586041f6a2
sayantansardar123 <sayantanardar2003@gmail.com>
2024-09-01 22:43:25 +0530

    add

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -5,9 +5,13 @@ import { Link } from 'react-router-dom'
 const Item = (props) =>{
   return (
     <div className='item'>
-      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
+      <div className='image-container'> 
+       <Link  to={`/product/${props.id}`}>
+         <img onClick={window.scrollTo(0,0)} src={props.image} alt="" />
+       </Link>
+      </div>
       <p>{props.name}</p>
-      <div className="item-prices">
+       <div className="item-prices">
         <div className="item-price-new">
           Rs.{props.new_price}
         </div>
