82f073f9999a3b855b76d976f08f3fde964e8fd2
Pratik0192 <pratikchakraborty019@gmail.com>
2024-09-03 00:25:10 +0530

    product card

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -8,7 +8,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography'; 
 import { Button } from '@mui/material';
-import { Height } from '@mui/icons-material';
+import { Star } from '@mui/icons-material';
 
 const Item = (props) => {
   const { 
@@ -32,6 +32,7 @@ const Item = (props) => {
   const buttonStyle = { 
     width: '100%',
     marginTop: '0px',
+    marginBottom: '23px',
     color: '#ff4141', 
     borderColor: '#ff4141',
     fontWeight: '600', 
@@ -52,13 +53,21 @@ const Item = (props) => {
         />
       </Link>
       <CardContent>
+        <Typography style={{marginTop:'-10px'}}>
+          4.5 <Star sx={{color:'#ff4141', marginBottom:'-5px'}} /> | 172
+        </Typography>
         <div className="item-info">
           {!isHovered ? (
-            <Typography 
-              style={{ fontSize: "16px", marginTop: "0px",fontWeight: "600",color: "#282c3f" }}  
-            >
-              {props.name}
-            </Typography>
+            <>
+              <Typography style={{fontSize:'16px', fontWeight:'700', color:'#282c3f', display:'block', marginBottom:'60px'}}>
+                Roadster
+              </Typography>
+              <Typography 
+                style={{ fontSize: "14px", marginTop: "0px", marginLeft:'-80px' ,fontWeight: "400",color: "#535766", whiteSpace:'nowrap', overflow:'hidden', textOverflow:'ellipsis', maxWidth:'260px',display:'block'}}  
+              >
+                {props.name}
+              </Typography>
+            </>
             ) : (
             <Button
               variant="outlined"
@@ -75,17 +84,20 @@ const Item = (props) => {
           <div className="item-prices">
             <Typography 
               variant='body1' 
-              style={{ fontSize: "16px", fontWeight: "600", color: "#282c3f" }}
+              style={{ fontSize: "16px", fontWeight: "600", color: "#282c3f", marginTop:'-20px'}}
               className='item-prices-new'
             >
               Rs.{props.new_price}
             </Typography>
             <Typography 
               variant='body2' 
-              style={{ fontSize: "14px", fontWeight: "500", color: "#7e818c", textDecoration: "line-through" }}
+              style={{ fontSize: "14px", fontWeight: "500", color: "#7e818c", marginTop:'-18px',marginLeft:'-1px' ,textDecoration: "line-through" }}
               className='item-prices-old'
             >
               Rs.{props.old_price}
+            </Typography>
+            <Typography style={{fontSize:'14px', marginLeft:'-5px', color:'#ff905a', marginTop:'-19px'}} >
+              (50% off)
             </Typography>
           </div>
         </div>
