3071c06dd0f23a280f24a8397da7f4a34b0dfd66
Vinay sahal <vinayrahul.sahal@gmail.com>
2024-09-15 11:20:27 +0530

    overview and profile updated

--- src/Components/ProfileComponent/ProfileComponent.jsx
+++ src/Components/ProfileComponent/ProfileComponent.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography, Avatar } from '@mui/material';
 
-const ProfileComponent = () => {
+const ProfileComponent = ({ name, mobileNumber, email, gender }) => {
   return (
-    <div>
-        profile details 
-    </div>
-  )
-}
+    <Box sx={{ padding: '80px' }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+        <Avatar sx={{ mr: 2 }}>V</Avatar>
+        <Typography variant="h5" sx = {{fontSize:"30px"}}>
+          Profile Details
+        </Typography>
+      </Box>
 
-export default ProfileComponent
+      <Box sx={{ marginBottom: '10px' }}>
+        <Typography variant="subtitle1" sx={{ fontWeight: '500' }}>
+          Full Name:
+        </Typography>
+        <Typography>{name || "N/A"}</Typography>
+      </Box>
+
+      <Box sx={{ marginBottom: '10px' }}>
+        <Typography variant="subtitle1" sx={{ fontWeight: '500' }}>
+          Mobile Number:
+        </Typography>
+        <Typography>{mobileNumber || "N/A"}</Typography>
+      </Box>
+
+      <Box sx={{ marginBottom: '10px' }}>
+        <Typography variant="subtitle1" sx={{ fontWeight: '500' }}>
+          Email:
+        </Typography>
+        <Typography>{email || "N/A"}</Typography>
+      </Box>
+
+      <Box sx={{ marginBottom: '10px' }}>
+        <Typography variant="subtitle1" sx={{ fontWeight: '500' }}>
+          Gender:
+        </Typography>
+        <Typography>{gender || "N/A"}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default ProfileComponent;
