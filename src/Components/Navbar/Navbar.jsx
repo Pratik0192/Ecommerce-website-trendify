ad8e5561eb26f3e330264608ad35e84cd4690c4a
Pratik0192 <pratikchakraborty019@gmail.com>
2024-09-21 14:55:44 +0530

    some changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,19 +2,65 @@ import React, { useContext, useRef, useState, useCallback } from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import { Link } from 'react-router-dom';
-import dropdown_icon from '../Assets/nav_dropdown.png';
-import { AiOutlineHeart } from 'react-icons/ai';
-import { BiUser } from 'react-icons/bi';
-import { BsBag } from 'react-icons/bs';
-import { BiSearch } from 'react-icons/bi';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
+
 import Tooltip from '@mui/material/Tooltip';
 import UserProfileDropdown from '../UserProfileDropdown/UserProfileDropdown';
 import { useDispatch, useSelector } from 'react-redux';
 import { productActions } from '../../store/productsSlice';
 import { debounce } from 'lodash';
+import { AppBar, Toolbar, IconButton, Typography, InputBase, Box, Button } from '@mui/material';
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '35ch',
+      '&:focus': {
+        width: '45ch',
+      },
+    },
+  },
+}));
+
+const linkButtonStyle = {
+  color:'#000',
+}
 
 const Navbar = () => {
   const [menu, setMenu] = useState('shop');
@@ -58,87 +104,50 @@ const Navbar = () => {
   const totalWishlistItems = wishlist.length;
 
   return (
-    <div className='navbar'>
-      <div className="nav-logo">
-        <Link to='/'>
-          <img src={logo} alt="Logo" />
-        </Link>
-      </div>
-      <img className='nav-dropdown' onClick={dropdown_toggle} src={dropdown_icon} alt="Dropdown Icon" />
-      <ul ref={menuRef} className="nav-menu">
-        {/* <li onClick={() => handlePageRouteChange('shop')}>
-          <Link className="nav-item-link" to='/'>Shop</Link>{menu === "shop" ? <hr /> : <></>}
-        </li> */}
-        <li onClick={() => handlePageRouteChange('mens')}>
-          <Link className="nav-item-link" to='/mens'>Men</Link>{menu === "mens" ? <hr /> : <></>}
-        </li>
-        <li onClick={() => handlePageRouteChange('womens')}>
-          <Link className="nav-item-link" to='/womens'>Women</Link>{menu === "womens" ? <hr /> : <></>}
-        </li>
-        <li onClick={() => handlePageRouteChange('kids')}>
-          <Link className="nav-item-link" to='/kids'>Kids</Link>{menu === "kids" ? <hr /> : <></>}
-        </li>
-        <li onClick={() => handlePageRouteChange('home&living')}>
-          <Link className="nav-item-link" to='/home&living'>Home & Living</Link>{menu === "home&living" ? <hr /> : <></>}
-        </li>
-        <li onClick={() => handlePageRouteChange('laptop')}>
-          <Link className="nav-item-link" to='/laptop'>Laptops</Link>{menu === "laptop" ? <hr /> : <></>}
-        </li>
-        <li onClick={() => handlePageRouteChange('mobile&tablet')}>
-          <Link className="nav-item-link" to='/mobile&tablet'>Mobiles</Link>{menu === "mobile&tablet" ? <hr /> : <></>}
-        </li>
-      </ul>
-      <div className="search">
-        <BiSearch className='search-icon' />
-        <input 
-          type="text" 
-          placeholder="search for product, brands and more"
-          onChange={handleSearchChange}
-        />
-      </div>
-      <div className="nav-login-cart">
-        <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-          <Tooltip title="Account settings">
-            <IconButton
-              onClick={handleClick}
-              size="small"
-              sx={{ ml: 2, maxWidth: "40px"}}
-              aria-controls={open ? 'account-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-            >
-              <div className='nav-icon-container'>
-                <BiUser className='nav-icon' />
-                <p>Profile</p>
-              </div>
-            </IconButton>
-          </Tooltip>
+    <AppBar position="sticky" sx={{backgroundColor:'#ffffff'}}>
+      <Toolbar>
+        {/* Logo */}
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1, color:'#000' }}>
+          MyLogo
+        </Typography>
+
+        {/* Navigation Links */}
+        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Button sx={linkButtonStyle}>Men</Button>
+          <Button sx={linkButtonStyle}>Women</Button>
+          <Button sx={linkButtonStyle}>Kids</Button>
+          <Button sx={linkButtonStyle}>Home & Living</Button>
+          <Button sx={linkButtonStyle}>Laptops</Button>
+          <Button sx={linkButtonStyle}>Mobiles</Button>
         </Box>
-        <UserProfileDropdown
-          anchorEl={anchorEl}
-          open={open}
-          handleClose={handleClose}
-        />
-        <Link to='/wishlist' className='nav-link'>
-          <div className='nav-icon-container'>
-            <AiOutlineHeart className='nav-icon' />
-            <p>Wishlist</p>
-          </div>
-          {totalWishlistItems > 0 && (
-            <div className="nav-wishlist-count">{totalWishlistItems}</div>
-          )}
-        </Link>
-        <Link to='/cart' className='nav-link'>
-          <div className='nav-icon-container'>
-            <BsBag className='nav-icon' />
-            <p>Cart</p>
-          </div>
-        </Link>
-        {cartLength > 0 && (
-          <div className="nav-cart-count">{cartLength}</div>
-        )}
-      </div>
-    </div>
+
+        {/* Search Bar */}
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search for product, brands and more"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Search>
+
+        {/* Account Icon */}
+        <IconButton size="large">
+          <AccountCircle />
+        </IconButton>
+
+        {/* Wishlist Icon */}
+        <IconButton size="large">
+          <FavoriteBorderIcon />
+        </IconButton>
+
+        {/* Cart Icon */}
+        <IconButton size="large">
+          <ShoppingBagIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 }
 
